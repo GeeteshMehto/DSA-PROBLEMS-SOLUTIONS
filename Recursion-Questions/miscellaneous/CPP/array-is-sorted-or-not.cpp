24ea0ce4db4f69c0302bc86472dd97010c2358b9
GeeteshMehto <geeteshmehto018@gmail.com>
2024-01-03 22:10:32 +0530

    Added misc question in array

---
+++ Recursion-Questions/miscellaneous/CPP/array-is-sorted-or-not.cpp
@@ -0,0 +1,14 @@
+bool isSorted(int arr[], int size){
+    if(size==0 || size==1){
+        return true;
+    }
+    if (arr[0]>arr[1])
+    {
+        return false;
+    }
+    else{
+        return isSorted(arr+1, size-1);
+    }
+    
+
+}
