2e84f7b61bbc4224b093d7206f178b55d0430791
GeeteshMehto <geeteshmehto018@gmail.com>
2024-01-05 13:41:00 +0530

    Quick sort added

---
+++ Recursion-Questions/miscellaneous/CPP/quick-sort.cpp
@@ -0,0 +1,57 @@
+int partition(int arr[], int s, int e)
+{
+
+    int pivot = arr[s];
+    int cnt = 0;
+
+    for (int i = s + 1; i <= e; i++)
+    {
+        if (arr[i] <= pivot)
+        {
+            cnt++;
+        }
+    }
+
+    // place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
+
+    int i = s, j = e;
+
+    while (i < pivotIndex && j > pivotIndex)
+    {
+        while (arr[i] <= pivot)
+        {
+            i++;
+        }
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+
+        if (i <= pivotIndex && j >= pivotIndex)
+        {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotIndex;
+}
+
+void quickSort(int arr[], int s, int e)
+{
+    if (s > e)
+    {
+        return;
+    }
+
+    // partion
+    int p = partition(arr, s, e);
+
+    // sort left part
+
+    quickSort(arr, s, p - 1);
+
+    // sort right part
+
+    quickSort(arr, p + 1, e);
+}
