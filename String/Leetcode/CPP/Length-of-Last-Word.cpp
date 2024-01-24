8215256a8c20f5848ec7225df43479eb660a07fb
GeeteshMehto <geeteshmehto018@gmail.com>
2024-01-24 22:33:10 +0530

    added in string

---
+++ String/Leetcode/CPP/Length-of-Last-Word.cpp
@@ -0,0 +1,24 @@
+/*
+Question Link:- https://leetcode.com/problems/length-of-last-word/description/
+*/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        // Count the length of the last word
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
