792512a29e48d5fb2100186af2e0854b0ab5ea83
GeeteshMehto <geeteshmehto018@gmail.com>
2024-03-08 23:38:54 +0530

    added in queue

---
+++ Queue/Leetcode/CPP/First-Unique-Character-in-a-String.cpp
@@ -0,0 +1,25 @@
+/*
+Question Link:- https://leetcode.com/problems/first-unique-character-in-a-string/description/
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> freqMap;
+
+        // Count frequency of characters
+        for (char c : s) {
+            freqMap[c]++;
+        }
+
+        // Find the first character with frequency 1
+        for (int i = 0; i < s.length(); ++i) {
+            if (freqMap[s[i]] == 1) {
+                return i;
+            }
+        }
+
+        // If no such character found
+        return -1;
+    }
+};
