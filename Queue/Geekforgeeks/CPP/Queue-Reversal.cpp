8ea5dea98e4a67328e7c7f0502690cd3ebb99be0
GeeteshMehto <geeteshmehto018@gmail.com>
2024-03-09 02:06:31 +0530

    added in gfg

---
+++ Queue/Geekforgeeks/CPP/Queue-Reversal.cpp
@@ -0,0 +1,28 @@
+/*
+Question Link:- https://www.geeksforgeeks.org/problems/queue-reversal/1
+*/
+
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        stack<int> s;
+        
+        while(!q.empty()){
+            int element = q.front();
+            q.pop();
+            s.push(element);
+        }
+        
+        while(!s.empty()){
+            int element = s.top();
+            s.pop();
+            q.push(element);
+        }
+        
+        return q;
+    }
+    
+    
+};
