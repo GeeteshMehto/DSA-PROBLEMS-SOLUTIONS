514ccdb633bb55406700c8d9f570573081f2f541
GeeteshMehto <geeteshmehto018@gmail.com>
2024-04-09 23:23:29 +0530

    added

---
+++ Array-Questions/Leetcode/CPP/time Needed to Buy Tickets.cpp
@@ -0,0 +1,30 @@
+/*
+Question link:- https://leetcode.com/problems/time-needed-to-buy-tickets
+*/
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n = tickets.size();
+        int time = 0;
+
+        // Simulate the process until the person at position k finishes buying
+        // tickets
+        while (tickets[k] > 0) {
+            for (int i = 0; i < n; ++i) {
+                if (tickets[i] > 0) {
+                    tickets[i]--; // Buy a ticket
+                    time++;       // Increment time by 1 second
+                    if (i == k &&
+                        tickets[k] == 0) // Check if the person at position k
+                                         // has finished buying tickets
+                        return time;
+                }
+            }
+        }
+
+        return time; // This line is unreachable, as the loop above will always
+                     // exit when the person at position k finishes buying
+                     // tickets
+    }
+};
